4872ed9b1d17890af6cf4be20135be77f55d34a0
Jai Gaignery <jaigaignery@gmail.com>
2023-12-04 21:51:11 -0500

    Removed globals import

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Link from "next/link";
-import "./globals.css";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-4 px-2">
-      <div className="font-var(--custom-font)">J</div>
+      <div className="bg-yellow"></div>
       <div className="flex items-center gap-[1ch]">Jai.dev</div>
       <div className="flex items-center gap-[1ch]">
         <Link href="#" className="link">
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <p>test</p>
     </>
   );
 }
