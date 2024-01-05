03273253eb739c01263eff2842c742e421d2bcb5
Jai Gaignery <jaigaignery@gmail.com>
2024-01-04 21:56:30 -0500

    Project box foundation fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,21 +3,10 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center py-4 px-2">
+    <nav className="flex justify-center mx-auto items-center py-8 px-2">
       <div className="flex items-center gap-[1ch]">
         <div className="w-5 h-5 bg-yellow-400 rounded-full" />
-        <span className="text-sm font-semibold tracking-widest">JAI.DEV</span>
-      </div>
-      <div className="flex justify-between gap-12 text-md text-zinc-400">
-        <Link href="#" className="text-black font-medium">
-          Portfolio
-        </Link>
-        <Link href="#" className="text-black font-medium">
-          Resume
-        </Link>
-        <Link href="#" className="text-black font-medium">
-          Contact
-        </Link>
+        <span className="text-lg font-semibold tracking-widest">JAI.DEV</span>
       </div>
     </nav>
   );
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Arrow from "../public/arrow.svg";
+import Image from "next/image";
 
 interface Props {
   name?: string | undefined;
   description?: string | undefined;
   imageUrl?: string | undefined;
   bgColour?: string | undefined;
+  altTag?: string | undefined;
   dark?: boolean | undefined;
 }
 
@@ -14,6 +16,7 @@ const ProjectCard: React.FC<Props> = ({
   description = "This is an amazing block",
   imageUrl = "/project-1.png",
   bgColour = "#e4e4e7",
+  altTag = "project image",
   dark = false,
 }) => {
   return (
@@ -21,10 +24,7 @@ const ProjectCard: React.FC<Props> = ({
       className={`h-[30rem] rounded-3xl overflow-hidden ${dark ? "dark" : ""}`}
       style={{ background: `${bgColour}` }}
     >
-      <div
-        className="h-full w-full px-10 py-6 duration-[500ms] transition-all ease-in-out hover:scale-105 bg-cover bg-no-repeat bg-center"
-        style={{ backgroundImage: `url('$imageUrl')` }}
-      >
+      <div className="h-full w-full px-10 py-6 duration-[500ms] transition-all ease-in-out hover:scale-105 bg-cover bg-no-repeat bg-center">
         <div className="flex justify-between">
           <div>
             <h2 className="font-medium text-lg dark:text-white">{name}</h2>
@@ -35,6 +35,9 @@ const ProjectCard: React.FC<Props> = ({
           <div className="h-12 w-12 bg-white rounded-full flex justify-center items-center cursor-pointer">
             <Arrow className="w-6 h-6" />
           </div>
+        </div>
+        <div className="h-full pb-6 flex justify-center items-center">
+          <Image src={imageUrl} width="350" height="350" alt={altTag} />
         </div>
       </div>
     </div>
