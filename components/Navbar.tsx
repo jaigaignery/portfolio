3bba547c2cfe5eefc221dfefd1e3b98a699d3251
Jai Gaignery <jaigaignery@gmail.com>
2024-01-08 00:15:57 -0500

    Changed site title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     <nav className="flex justify-center mx-auto items-center py-8 px-2">
       <div className="flex items-center gap-[1ch]">
         <div className="w-5 h-5 bg-yellow-400 rounded-full" />
-        <span className="text-lg font-semibold tracking-widest">JAI.DEV</span>
+        <span className="text-lg font-semibold tracking-widest">
+          JAI GAIGNERY
+        </span>
       </div>
     </nav>
   );
